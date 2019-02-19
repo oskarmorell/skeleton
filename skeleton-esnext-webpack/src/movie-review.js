51ee334bf326bc134f2d9456afcbf29b2e7a7a0c
Oskar Morell <guest.oskar.morell@hogia.se>
2019-02-19 13:01:54 +0100

    dialog

--- skeleton-esnext-webpack/src/movie-review.js
+++ skeleton-esnext-webpack/src/movie-review.js
@@ -2,31 +2,33 @@ import {DialogService} from 'aurelia-dialog';
 import {inject} from 'aurelia-framework';
 import {Prompt} from './display-review';
 
-export class moviereview{
-    ratings = [
-        {id:0, name:'1'},
-        {id:1, name:'2'},
-        {id:2, name:'3'},
-        {id:3, name:'4'},
-        {id:4, name:'5'}
-    ];
+@inject(DialogService)
 
-    genres = ['Comedy', 'Action', 'Thriller', 'Horror', 'Fantasy'];
-}
 export class App {
-    constructor(dialogService) {
-       this.dialogService = dialogService;
-    }
-    openModal() {
-       this.dialogService.open( {viewModel: Prompt, model: 'Are you sure?' }).then(response => {
-          console.log(response);
-             
-          if (!response.wasCancelled) {
-             console.log('OK');
-          } else {
-             console.log('cancelled');
-          }
-          console.log(response.output);
-       });
-    }
- };
+   constructor(dialogService) {
+      this.dialogService = dialogService;
+   }
+   openModal() {
+      this.dialogService.open( {viewModel: Prompt, model: 'Are you sure?' }).then(response => {
+         console.log(response);
+			
+         if (!response.wasCancelled) {
+            console.log('OK');
+         } else {
+            console.log('cancelled');
+         }
+         console.log(response.output);
+      });
+   }
+};
+export class moviereview{
+   ratings = [
+       {id:0, name:'1'},
+       {id:1, name:'2'},
+       {id:2, name:'3'},
+       {id:3, name:'4'},
+       {id:4, name:'5'}
+   ];
+
+   genres = ['Comedy', 'Action', 'Thriller', 'Horror', 'Fantasy'];
+}
